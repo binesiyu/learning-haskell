b432ee44a8d3ddc0ba523fc6838796ebbae1984f
binesiyu <yubinexsaber@gmail.com>
2018-12-18 23:49:21 +0800

    learning newtype

--- ch1/src/Lib.hs
+++ ch1/src/Lib.hs
@@ -20,6 +20,8 @@ data Person = Person { firstName :: FirstName
                       ,height :: Float
                      }deriving (Show)
 
+newtype PersonNew = PersonNew Person deriving(Show)
+
 testPerson = Person "fname" "lname" 39 170
 
 firstName' :: Person -> FirstName
@@ -33,6 +35,21 @@ arge' (Person _ _ x _) = x
 
 height' :: Person -> Float
 height' (Person _ _ _ h) = h
+
+data PersonG a b c = PersonG {firstNameG :: a
+                         ,lastNameG ::a
+                         ,argeG :: b
+                         ,heightG :: c
+                         }deriving (Show)
+
+type PersonString = PersonG String Int Float
+type PersonString' = PersonG String Integer Float
+
+testPersonString1 :: PersonString
+testPersonString1 = PersonG "1firstName" "1lastName" 35 178.0
+
+testPersonString2 :: PersonString'
+testPersonString2 = PersonG "2firstName" "2lastName" 35 178.0
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
