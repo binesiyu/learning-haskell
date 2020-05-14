674e03cd3ea87dd9337dfbeb019701cc51bc2da6
yubin <1960028060@qq.com>
2020-05-14 15:05:41 +0800

    test-case

--- ch06-quickcheckls/src/Lib.hs
+++ ch06-quickcheckls/src/Lib.hs
@@ -1,6 +1,13 @@
 module Lib
-    ( someFunc
-    ) where
+    (qsort) where
+
+import Data.List
+
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
+    where lhs = filter  (< x) xs
+          rhs = filter (>= x) xs
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- ch06-quickcheckls/test/Spec.hs
+++ ch06-quickcheckls/test/Spec.hs
@@ -1,2 +1,66 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE TemplateHaskell #-}
+
+import Lib
+import Data.List
+import Test.QuickCheck
+
+prop_minimum xs         = not (null xs) ==> head (qsort xs) == minimum xs
+prop_idempotent xs = qsort (qsort xs) == qsort xs
+
+prop_ordered xs = ordered (qsort xs)
+    where ordered []       = True
+          ordered [x]      = True
+          ordered (x:y:xs) = x <= y && ordered (y:xs)
+
+prop_permutation xs = permutation xs (qsort xs)
+    where permutation xs ys = null (xs \\ ys) && null (ys \\ xs)
+
+prop_maximum xs         =
+    not (null xs) ==>
+        last (qsort xs) == maximum xs
+
+prop_append xs ys       =
+    not (null xs) ==>
+    not (null ys) ==>
+        head (qsort (xs ++ ys)) == min (minimum xs) (minimum ys)
+
+prop_sort_model xs      = sort xs == qsort xs
+
+--------------------------
+return []
+
+-- main = runTests
+
+anal :: Args
+anal = Args
+    { replay = Nothing
+    , maxSuccess = 1000
+    , maxDiscardRatio = 1
+    , maxSize = 1000
+    , chatty = True
+    , maxShrinks = 1
+    }
+
+minimal :: Args
+minimal = Args
+    { replay = Nothing
+    , maxSuccess = 200
+    , maxDiscardRatio = 1
+    , maxSize = 200
+    , chatty = True
+    , maxShrinks = 1
+    }
+
+runTests :: Args -> IO Bool
+runTests args = $forAllProperties $ quickCheckWithResult args
+
+main :: IO Bool
+main = do
+    putStrLn "Choose test depth"
+    putStrLn "1. Anal"
+    putStrLn "2. Minimal"
+    runTests minimal
+    -- depth <- readLn
+    -- if depth == 1
+    --     then runTests anal
+    -- else runTests minimal
